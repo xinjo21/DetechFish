af5415a365a58bfe3afe0374b2572a97139ac998
xinjo21 <xinjo21>
2021-10-19 11:04:32 +0800

    added ReactJs + MaterialUI

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+/* import './App.css'; */
+import Button from '@mui/material/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test world</h1>
+    </div>
+  );
+}
+
+export default App;
