7e667dee2fddf5c5719a89f3a6fe390329bfc783
xinjo21 <xinjo21>
2021-11-17 10:47:33 +0800

    moved API to api folder

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,66 +9,8 @@ import Sync from './pages/Sync'
 import Map from './pages/Map'
 import err404 from './pages/err404'
 
-
-function getDate() {
-  const date = new Date()
-
-  var currentDay = date.getDay()
-  var currentMonth = date.getMonth()
-  var currentDate = date.getDate()
-  var currentYear = date.getFullYear()
-
-  var month = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ]
-
-  var day = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ]
-
-  var curDate = day[currentDay] + ", " + month[currentMonth] + " " + currentDate + ", " + currentYear
-
-  return curDate
-}
-
-function getTime() {
-  const date = new Date()
-
-  var hr = date.getHours()
-  var mn = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes()
-  var sec = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds()
-  
-  var time
-  
-  if (hr > 12 || hr === 12) {
-    hr = (hr !== 12 ? (hr - 12) : 12 )
-    time = (hr < 10 ? '0' : '') + hr + ':' + mn + ":" + sec + " PM"
-  } else if (hr === 0 || hr < 12 ) {
-    hr = (hr !== 0 ? hr : 12)
-    time = hr + ':' + mn + ":" + sec + " AM"
-  } /* else {
-    time = (hr < 10 ? '0' : '') + hr + ':' + mn + ":" + sec + " AM"
-  } */
-  return time
-}
-
+// function imports:
+import { getTime, getDate } from './functions/date'
 
 function App() {
   const [time, setTime] = useState(getTime())
